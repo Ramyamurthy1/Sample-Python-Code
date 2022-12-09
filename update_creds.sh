f5ad934ad40c95b1493600c1f428ef8100be9ef3
Ramyamurthy1 <71689655+Ramyamurthy1@users.noreply.github.com>
2022-12-09 12:32:51 +0530

    Update update_creds.sh

--- update_creds.sh
+++ update_creds.sh
@@ -1,14 +1,20 @@
-EKS_NAMESPACE="cyswy001n-ns-ib-orch-001"
-EKS_CLUSTER="dish-test"
+#############################################################################################################################
+#   shell sscript will login to kubereners EKS Cluster and fetch teh required secret or any other information required #
+#   The secret can then be updated in a file using sed commands                                                                                                                       #
+#                                                                                                                           #
+#############################################################################################################################
 
-#aws eks --region us-east-2 update-kubeconfig --name $EKS_CLUSTER
+
+EKS_NAMESPACE="namespace"
+EKS_CLUSTER="test"
+
+#aws eks --region AWS-REGION update-kubeconfig --name $EKS_CLUSTER
 #kubectl get $EKS_NAMESPACE
 
 echo "Inside update script"
 
-kubectl get secret ib-orc-230-strimzi-001-cluster-ca-cert -o "jsonpath={.data['ca\.crt']}" -n $EKS_NAMESPACE | base64 -d > ca.crt
+kubectl get secret ib-orc-230-strimzi-001 -o "jsonpath={.data['ca\.crt']}" -n $EKS_NAMESPACE | base64 -d > ca.crt
 cat ca.crt
 
 kafka_password=`kubectl get secret cp4na-o-kafka-user -n $NAMESPACE -o jsonpath='{ .data.sasl\.jaas\.config }' | base64 -d|grep password|awk '{print $NF}'|sed -e 's/password=//g'|tr -d '"'|sed "s/;/',/"`
 echo "echo kafka password is " $kafka_password
-echo "updating the passwd in kafka file"
